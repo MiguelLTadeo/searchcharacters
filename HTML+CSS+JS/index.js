1b71a8192e7e3b1b80fd2cc27d1c38724a63f636
MiguelLTadeo <migueltadeo.aluno@unipampa.edu.br>
2024-12-20 09:43:43 -0300

    get all function added

--- HTML+CSS+JS/index.js
+++ HTML+CSS+JS/index.js
@@ -1,11 +1,29 @@
-async function getcharacter() {
+function buttonchoice(value) {
   const name = document.getElementById("charactername").value;
-  const charimage = document.getElementById("charimage");
-  const charname = document.getElementById("charname");
-  fetch(`https://eldenring.fanapis.com/api/bosses?name=${name}`)
+  searchurl = `https://eldenring.fanapis.com/api/bosses?name=${name}`;
+  allurl = `https://eldenring.fanapis.com/api/bosses?limit=106`;
+  if (value === 0) {
+    getcharacter(searchurl);
+  }
+  if (value === 1) {
+    getcharacter(allurl);
+  }
+}
+
+async function getcharacter(url) {
+  fetch(url)
     .then((data) => data.json())
     .then((body) => {
-      charimage.src = body.data[0].image;
-      charname.textContent = body.data[0].name;
+      const characterlist = document.getElementById("Characterslist");
+      characterlist.innerHTML = body.data
+        .map(
+          (i) => `
+      <div>
+        <h1>${i.name}</h1>
+        <br/>
+        <img src = "${i.image}" alt ="${i.image}"/>
+      </div>`
+        )
+        .join("");
     });
 }
