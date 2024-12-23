b4d3436804341f06831d24d0c18294a6aaa7c8a0
MiguelLTadeo <migueltadeo.aluno@unipampa.edu.br>
2024-12-23 13:17:20 -0300

    format the search and all button function

--- HTML+CSS+JS/index.js
+++ HTML+CSS+JS/index.js
@@ -1,30 +1,46 @@
-function buttonchoice(value) {
-  const name = document.getElementById("charactername").value;
-  searchurl = `https://eldenring.fanapis.com/api/bosses?name=${name}`;
-  allurl = `https://eldenring.fanapis.com/api/bosses?limit=106`;
-  if (value === 0) {
-    getcharacter(searchurl);
+async function getcharacter(button) {
+  let name = document.getElementById("charactername").value;
+  if (button === 1) {
+    name = "";
   }
-  if (value === 1) {
-    getcharacter(allurl);
-  }
-}
 
-async function getcharacter(url) {
-  fetch(url)
+  fetch(`https://eldenring.fanapis.com/api/bosses?limit=106&name=${name}`)
     .then((data) => data.json())
     .then((body) => {
       const characterlist = document.getElementById("Characterslist");
-      characterlist.innerHTML = body.data
-        .filter((i) => i.image !== null)
-        .map(
-          (i) => `
+      const speccharacter = document.createElement("speccharacter");
+      speccharacter.classList.add("speccharacter");
+      const allcharacter = document.createElement("allcharacter");
+      allcharacter.classList.add("allcharacter");
+      if (button === 0) {
+        characterlist.innerHTML = "";
+        speccharacter.innerHTML = body.data
+          .filter((i) => i.image !== null)
+          .map(
+            (i) => `
       <div class="charactercontainer">
         <h1>${i.name}</h1>
         <br/>
         <img src = "${i.image}" alt ="${i.image}" class="imagechar"/>
       </div>`
-        )
-        .join("");
+          )
+          .join("");
+        characterlist.appendChild(speccharacter);
+      }
+      if (button === 1) {
+        characterlist.innerHTML = "";
+        allcharacter.innerHTML = body.data
+          .filter((i) => i.image !== null)
+          .map(
+            (i) => `
+      <div class="charactercontainer">
+        <h1>${i.name}</h1>
+        <br/>
+        <img src = "${i.image}" alt ="${i.image}" class="imagechar"/>
+      </div>`
+          )
+          .join("");
+        characterlist.appendChild(allcharacter);
+      }
     });
 }
